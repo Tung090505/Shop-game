cb3e254f3a1f16f1323019cef9ba7f1fc93ff000
Tung090505 <2331540153@vaa.edu.vn>
2025-12-23 21:23:34 +0700

    Fix hardcoded API URL in index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,17 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
-});
-
-// Add a request interceptor to include the auth token
-API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
-    }
-    return req;
-});
+import API from './config'; // Sử dụng instance đã cấu hình URL chuẩn (dy16)
 
 // Auth Services
 export const login = (formData) => API.post('/user/login', formData);
