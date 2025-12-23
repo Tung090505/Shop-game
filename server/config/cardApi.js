a6de9ccd5b7f7e572032295bbec32184e5e0dc1d
Tung090505 <2331540153@vaa.edu.vn>
2025-12-23 22:38:33 +0700

    Cấu hình API nạp thẻ GachThe1s

--- server/config/cardApi.js
+++ server/config/cardApi.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PARTNER_ID: 'YOUR_PARTNER_ID', // Tùng thay Partner ID lấy trên GachThe1s vào đây
-    PARTNER_KEY: 'YOUR_PARTNER_KEY', // Tùng thay Partner Key lấy trên GachThe1s vào đây
-    API_URL: 'https://gachthe1s.com/chargingws/v2', // ĐÃ SỬA: Không được sửa dòng này
-    CALLBACK_URL: 'http://your-domain.com/api/webhooks/card' // Link web của bạn
+    PARTNER_ID: '60231688480', // Lấy từ GachThe1s
+    PARTNER_KEY: '5f498182463e4226ddb618ac1289e57c', // Lấy từ GachThe1s
+    API_URL: 'https://gachthe1s.com/chargingws/v2',
+    CALLBACK_URL: 'https://shop-game-dy16.onrender.com/api/webhooks/card' // URL nhận kết quả từ GachThe1s
 };
