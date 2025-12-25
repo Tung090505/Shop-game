f9f1883268b4e7b70f32f47ab61046baab680f4c
Tung090505 <2331540153@vaa.edu.vn>
2025-12-25 21:57:51 +0700

    Fix: Allow GET request on card webhook for verification

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -6,5 +6,6 @@ router.post('/bank', webhookController.handleBankWebhook);
 
 // Webhook cho bên gạch thẻ cào
 router.post('/card', webhookController.handleCardWebhook);
+router.get('/card', (req, res) => res.status(200).send('Webhook is active'));
 
 module.exports = router;
